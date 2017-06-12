5cb5e734986e9c314ad9aa6a274fdce5a9a3d7b1
Top Mora <manager@topmora.com>
2017-06-12 06:38:37 +0200

    kipa - support lang

--- src/ressources/lang/en/overall.php
+++ src/ressources/lang/en/overall.php
@@ -3,5 +3,7 @@ return [
 	"welcome" => "Bienvenue",
 	"keywords" => "Application facebook",
 	"description" => "Description par défaut",
-	"social_login" => "S'inscrire"
+	"social_login" => "S'inscrire",
+	"first" => "Début",
+	"last" => "Dernier"
 ];
